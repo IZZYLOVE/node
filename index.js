e16c9b8e01f7d2ab6a2f9a3df157a00b1ec5f406
IZZYLOVE <izzylovu@gmail.com>
2023-03-07 14:46:58 +0100

    reading a file

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const http = require("http");
-const fs = require("fs");
+const http = require('http');
+const fs = require('fs');
 
-require("dotenv").config()
+require('dotenv').config()
 //const port = process.env.PORT && 8000
 const port = process.env.PORT || 8000
 
+//res.writeHead(200, {"content-Type": "text/plain"})
 http.createServer(
     (req, res) => {
-        fs.readFile("about.html", (err, data) => {
-            res.writeHead(200, {"content-Type": "text/plain"})
+        fs.readFile('about.html', (err, data) => {
+            res.writeHead(200, {'Content-Type': 'text/html'})
             res.write("Hello!")
+            res.write(data)
             res.end() 
         })
     }
