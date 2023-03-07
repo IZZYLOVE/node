6ddc796e8a59e718b735229c3aecac13ef924adf
IZZYLOVE <izzylovu@gmail.com>
2023-03-07 14:27:54 +0100

    about.html updated

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const http = require("http");
+const fs = require("fs");
+
 require("dotenv").config()
 //const port = process.env.PORT && 8000
 const port = process.env.PORT || 8000
 
 http.createServer(
     (req, res) => {
-        res.writeHead(200, {"content-Type": "text/plain"})
-        res.write("Hello!")
-        res.end() 
+        fs.readFile("about.html", (err, data) => {
+            res.writeHead(200, {"content-Type": "text/plain"})
+            res.write("Hello!")
+            res.end() 
+        })
     }
 ).listen(port, () => console.log("server extablished! "))
